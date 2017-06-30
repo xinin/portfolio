96ad94a46a37ae697d3eebaa7146df7f967fc87a
diego.prieto <diego.prieto@beeva.com>
2017-06-30 11:49:32 +0200

    gulp mejorado, todo en dist

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var removeFiles = require('gulp-remove-files');
 var pkg = require('./package.json');
 
 // Set the banner content
@@ -20,7 +21,7 @@ gulp.task('less', function() {
     return gulp.src('less/main.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -28,13 +29,19 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/main.css')
+    gulp.src('dist/css/main.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+    gulp.src('dist/css/main.css')
+        .pipe(removeFiles());
+});
+
+gulp.task('css', ['less', 'minify-css'], function () {
+
 });
 
 // Minify JS
@@ -43,7 +50,7 @@ gulp.task('minify-js', function() {
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -52,10 +59,10 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'));
+        .pipe(gulp.dest('dist/vendor/bootstrap'));
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'));
+        .pipe(gulp.dest('dist/vendor/jquery'));
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -65,31 +72,39 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
+        .pipe(gulp.dest('dist/vendor/font-awesome'));
+
+    gulp.src(['img/**/*', 'files/**/*', 'index.html']).pipe(gulp.dest('dist/img'));
+    gulp.src(['files/**/*']).pipe(gulp.dest('dist/files'));
+    gulp.src(['index.html']).pipe(gulp.dest('dist'));
+});
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: ''
+            baseDir: 'dist/'
         }
     })
 });
 
 // Dev task with browserSync
-gulp.task('dev', ['less', 'minify-css', 'minify-js', 'browserSync'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
+// gulp.task('dev', ['css', 'minify-js', 'browserSync'], function() {
+//     gulp.watch('less/*.less', ['less']);
+//     gulp.watch('css/*.css', ['minify-css']);
+//     gulp.watch('js/*.js', ['minify-js']);
+//     // Reloads the browser whenever HTML or JS files change
+//     gulp.watch('*.html', browserSync.reload);
+//     gulp.watch('js/**/*.js', browserSync.reload);
+// });
+
+gulp.task('serve', [ 'default', 'browserSync'], function () {
+    gulp.watch('less/*.less', ['css']);
     gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('index.html', ['copy']);
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('dist/**/*', browserSync.reload);
 });
-
-gulp.task('serve', ['default', 'dev']), function () {
-
-};
