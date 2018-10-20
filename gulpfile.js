26c6d328f58e027185ad75b40ecb61105d0a4f6e
diego.prieto <diego.prieto@gruposantander.com>
2018-10-20 19:13:13 +0200

    fix gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -58,28 +58,11 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('dist/vendor/bootstrap'));
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('dist/vendor/jquery'));
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('dist/vendor/font-awesome'));
-
-    gulp.src(['img/**/*', 'files/**/*', 'index.html']).pipe(gulp.dest('dist/img'));
+    gulp.src(['img/**/*']).pipe(gulp.dest('dist/img'));
     gulp.src(['files/**/*']).pipe(gulp.dest('dist/files'));
     gulp.src(['index.html']).pipe(gulp.dest('dist'));
     gulp.src(['robots.txt']).pipe(gulp.dest('dist'));
     gulp.src(['sitemap.xml']).pipe(gulp.dest('dist'));
-
 });
 
 // Run everything
